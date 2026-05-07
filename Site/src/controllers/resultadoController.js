b7a13684f9eeb753d1b6d2a28d8283fd6225469a
MatheusDTavares <matheus.tavares@sptech.school>
2026-05-07 15:58:42 -0300

    Terminando as Rotas para inserção dos dados da Dash

---
+++ Site/src/controllers/resultadoController.js
@@ -0,0 +1,99 @@
+var resultadoModel = require("../models/resultadoModel")
+
+function buscarResultadosIndividuais(req,res) {
+    var idUsuario = req.params.idUsuario;
+
+    console.log("Recuperando resultados em tempo real");
+
+    resultadoModel.buscarMedidasEmTempoReal(idUsuario).then(function (resultado){
+        if(resultado.length > 0) {
+            res.status(200).json(resultado);
+        }   else   {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscaros ultimos resultados.", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
+function buscarPilotos(req,res) {
+    resultadoModel.buscarResultadosGerais('p.r1', 'Piloto')
+    .then(function(piloto)  {
+        res.status(200).json(piloto);
+    })
+    .catch(function(erro)  {
+        console.log(erro);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
+function buscarTempo(req,res) {
+    resultadoModel.buscarResultadosGerais('p.r2', 'Tempo')
+    .then(function(tempo)  {
+        res.status(200).json(tempo);
+    })
+    .catch(function(erro)  {
+        console.log(erro);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
+function buscarInteresse(req,res) {
+    resultadoModel.buscarResultadosGerais('p.r3', 'Interesse')
+    .then(function(interesse)  {
+        res.status(200).json(interesse);
+    })
+    .catch(function(erro)  {
+        console.log(erro);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
+function buscarEquipe(req,res) {
+    resultadoModel.buscarResultadosGerais('p.r4', 'Equipe')
+    .then(function(equipe)  {
+        res.status(200).json(equipe);
+    })
+    .catch(function(erro)  {
+        console.log(erro);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
+function buscarCuriosidade(req,res) {
+    resultadoModel.buscarResultadosGerais('p.r5', 'Curiosidade')
+    .then(function(curiosidade)  {
+        res.status(200).json(curiosidade);
+    })
+    .catch(function(erro)  {
+        console.log(erro);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
+function buscarOpniao(req,res) {
+    resultadoModel.buscarResultadosGerais('p.r6', 'Opniao')
+    .then(function(opniao)  {
+        res.status(200).json(opniao);
+    })
+    .catch(function(erro)  {
+        console.log(erro);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
+
+
+module.exports = {
+
+    buscarResultadosIndividuais,
+    buscarPilotos,
+    buscarTempo,
+    buscarInteresse,
+    buscarEquipe,
+    buscarCuriosidade,
+    buscarOpniao
+
+}
--- Site/src/models/resultadoModel.js
+++ Site/src/models/resultadoModel.js
@@ -1,8 +1,50 @@
 var database = require("../database/config")
 
-function buscarResultadosGerais(resposta, apelido) {
+function buscarResultadosGerais(resposta,apelido) {
     var instrucaoSql = `
     SELECT ${resposta} as ${apelido}, count(*) as total
     FROM pesquisa
-    JOIN piloto p on pesquisa.r1 = p.id`
+    GROUP BY ${resposta}
+    ORDER BY ${resposta} ASC`;
+
+    var instrucaoSqlMaior = `
+    SELECT ${resposta} as ${apelido}, count(*) as total
+    FROM pesquisa
+    GROUP BY ${resposta}
+    ORDER BY ${resposta} ASC
+    LIMIT 1`;
+
+    var instrucaoSqlMenor = `
+    SELECT ${resposta} as ${apelido}, count(*) as total
+    FROM pesquisa
+    GROUP BY ${resposta}
+    ORDER BY ${resposta} ASC
+    LIMIT 1`;
+
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql)
+    .then(function(respostas)   {
+        return database.executar(instrucaoSqlMaior)
+        .then(function(maior) {
+            return database.executar(instrucaoSqlMenor)
+            .then(function(menor)   {
+                return [respostas, maior[0], menor[0]];
+            })
+        })
+    })
+    
+}
+
+
+function buscarResultadosIndividuais(idUsuario) {
+    var instrucaoSql = `
+    p.r1 as piloto
+    p.r2
+    p.r3
+    p.r4
+    p.r5
+    p.r6
+    FROM pesquisa p
+    WHERE p.fkUsuario = ${idUsuario}`
 }
