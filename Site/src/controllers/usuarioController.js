cfeae1d4e9cb7fd57df7e76b5845c971684f4c8d
vazflavia1910 <flavia.vaz@sptech.school>
2026-05-07 10:28:45 -0300

    Adicionando o css da dash

--- Site/src/controllers/usuarioController.js
+++ Site/src/controllers/usuarioController.js
@@ -1,5 +1,5 @@
 var usuarioModel = require("../models/usuarioModel");
-var aquarioModel = require("../models/aquarioModel");
+// var aquarioModel = require("../models/aquarioModel");
 
 function autenticar(req, res) {
     var email = req.body.emailServer;
@@ -20,15 +20,15 @@ function autenticar(req, res) {
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
                             
-                                if (resultadoAquarios.length > 0) {
+                                // if (resultadoAquarios.length > 0) {
                                     res.json({
                                         id: resultadoAutenticar[0].id,
                                         email: resultadoAutenticar[0].email,
                                         nome: resultadoAutenticar[0].nome,
                                         senha: resultadoAutenticar[0].senha,
-                                        aquarios: resultadoAquarios
+                                        // aquarios: resultadoAquarios
                                     });
-                                } 
+                                // } 
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
