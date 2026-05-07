514b50fc386caef6345f5a2acc1830dfdb96c2a5
vazflavia1910 <flavia.vaz@sptech.school>
2026-05-07 13:57:52 -0300

    Alterando script do banco

--- Site/src/database/script-tabelas.sql
+++ Site/src/database/script-tabelas.sql
@@ -2,7 +2,7 @@ Create database M1Racing;
 use M1Racing;
 
 create table usuario(
-IdUsuario int primary key auto_increment,
+id int primary key auto_increment,
 nome varchar(100),
 email varchar(100),
 senha varchar(100)
@@ -17,7 +17,12 @@ r4 varchar(30) not null,
 r5 varchar(30) not null,
 r6 varchar(30) not null,
 primary key(fkUsuario),
-
-
+constraint fk_usuario_pesquisa foreign key (fkUsuario) references usuario(id)
+constraint chk_r1 check()
+constraint chk_r2 check()
+constraint chk_r3 check()
+constraint chk_r4 check()
+constraint chk_r5 check()
+constraint chk_r6 check()
 
 );
