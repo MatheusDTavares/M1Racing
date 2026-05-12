b987ed1d9c3b6f0a64b1419088436c21745c3730
MatheusDTavares <matheus.tavares@sptech.school>
2026-05-12 17:46:54 -0300

    Adicionando Banco de dados atualizado

--- Site/src/database/script-tabelas.sql
+++ Site/src/database/script-tabelas.sql
@@ -8,21 +8,29 @@ email varchar(100),
 senha varchar(100)
 );
 
+create table piloto(
+    idPiloto int primary key auto_increment,
+    nome varchar(60)
+);
+
+create table equipe(
+    idEquipe int primary key auto_increment,
+    nome varchar(100)
+);
+
 create table pesquisa(
 fkUsuario int not null,
 r1 varchar(30) not null,
 r2 varchar(30) not null,
 r3 varchar(30) not null,
 r4 varchar(30) not null,
-r5 varchar(30) not null,
+r5 varchar(255) not null,
 r6 varchar(30) not null,
 primary key(fkUsuario),
-constraint fk_usuario_pesquisa foreign key (fkUsuario) references usuario(id)
-constraint chk_r1 check()
-constraint chk_r2 check()
-constraint chk_r3 check()
-constraint chk_r4 check()
-constraint chk_r5 check()
-constraint chk_r6 check()
-
+constraint fk_usuario_pesquisa foreign key (fkUsuario) references usuario(id),
+constraint chk_r1 check(r1 >= 1 and r1 <=22),
+constraint chk_r2 check(r2 in ('Novato', 'Amador', 'Intermediario', 'Sabio')),
+constraint chk_r3 check(r3 in ('Tudo', 'Treino', 'Qualificacao', 'Corrida', 'Nada')),
+constraint chk_r4 check(r4 >= 1 and r4 <= 11),
+constraint chk_r6 check(r6 in ('NaoGostei', 'Nada', 'Gostei'))
 );
