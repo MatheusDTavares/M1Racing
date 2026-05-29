44f673a50d5d6da1b76e0b2ab2f8a140e0dcfce5
MatheusDTavares <matheus.tavares@sptech.school>
2026-05-29 06:22:44 -0300

    adicionando script do banco

--- src/database/script-insert.sql
+++ src/database/script-insert.sql
@@ -47,3 +47,27 @@ INSERT INTO piloto (idPiloto, nome) VALUES
 (20, 'Sergio Pérez'),
 (21, 'Valtteri Bottas'),
 (22, 'Zhou Guanyu');
+
+INSERT INTO usuario (nome, email, senha) VALUES
+('Ana Silva', 'ana@email.com', '123456'),
+('Bruno Costa', 'bruno@email.com', '123456'),
+('Carla Mendes', 'carla@email.com', '123456'),
+('Diego Lima', 'diego@email.com', '123456'),
+('Eva Rocha', 'eva@email.com', '123456'),
+('Felipe Gomes', 'felipe@email.com', '123456'),
+('Gabriela Nunes', 'gabriela@email.com', '123456'),
+('Henrique Alves', 'henrique@email.com', '123456'),
+('Isabela Ferreira', 'isabela@email.com', '123456'),
+('João Pereira', 'joao@email.com', '123456');
+
+INSERT INTO pesquisa (fkUsuario, r1, r2, r3, r4, r5, r6) VALUES
+(1,  '16', 'Sabio',         'Tudo',        '9',  'Hamilton',    '3'),  -- Verstappen, Red Bull, Interlagos
+(2,  '12', 'Amador',        'Corrida',     '11', 'Senna',       '1'),  -- Norris, McLaren, Mônaco
+(3,  '13', 'Intermediario', 'Tudo',        '1',  'Schumacher',  '6'),  -- Hamilton, Mercedes, Spa
+(4,  '4',  'Novato',        'Qualificacao','4',  'Verstappen',  '2'),  -- Leclerc, Ferrari, Silverstone
+(5,  '16', 'Sabio',         'Tudo',        '9',  'Senna',       '3'),  -- Verstappen, Red Bull, Interlagos
+(6,  '12', 'Amador',        'Nada',        '11', 'Hamilton',    '5'),  -- Norris, McLaren, Suzuka
+(7,  '13', 'Sabio',         'Tudo',        '1',  'Lauda',       '1'),  -- Hamilton, Mercedes, Mônaco
+(8,  '18', 'Novato',        'Corrida',     '11', 'Verstappen',  '4'),  -- Piastri, McLaren, Monza
+(9,  '16', 'Intermediario', 'Treino',      '9',  'Vettel',      '3'),  -- Verstappen, Red Bull, Interlagos
+(10, '7',  'Novato',        'Corrida',     '9',  'Hamilton',    '2');  -- Bortoleto, Red Bull, Silverstone
